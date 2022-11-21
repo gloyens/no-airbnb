c8f0661c353b38a1763cd0bb294aaf431e911a8e
Michelle Modest <michelle.modest@aya.yale.edu>
2022-11-21 16:17:47 +0000

    added planets controller, views, and routes

---
+++ app/controllers/planets_controller.rb
@@ -0,0 +1,25 @@
+class PlanetsController < ApplicationController
+
+  def index
+    @planets = Planet.new
+  end
+
+  def show
+    @planet = Planet.find(params[:id])
+  end
+
+  def new
+    @planet = Planet.new
+  end
+
+  def create
+    @planet=Planet.new(planet_params)
+    @planet.save
+    redirect_to planet_path(@planet)
+  end
+
+  private
+  def planet_params
+    params.require(:planet).permit(:name, :description, :size, :image_url, :population, :rating, :price)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
+
+  devise_for :users
+
   root to: "pages#home"
+  get "/planets", to: "planet#index"
+  get "/planets/:id", to: "planet#show"
+  get "/planets/new", to: "planet#new"
+  post "planets", to: "planet#create"
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
